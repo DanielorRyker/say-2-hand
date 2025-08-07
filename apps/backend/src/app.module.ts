b871251643d22f2c0f1c90efde08989e64ec4367
DanielorRyker <hoangraper2017@gmail.com>
2025-08-08 01:38:28 +0700

    Setup and Connect Database || Repair Logo and Name Web

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGODB_URI || ''),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Connect to MongoDB successfully');
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  
+  const port = process.env.PORT || '';
+  
+  // Gọi listen() trước để khởi động server
+  await app.listen(port);
+  ``
+  // Sau đó mới gọi getUrl() để lấy địa chỉ
+  const url = await app.getUrl();
+  
+  logger.log(`Server is running on: http://localhost:${port}`);
 }
 bootstrap();
