9ced1513a2ac2f9e7ac5e73dbefc6171e1bd1e0c
DanielorRyker <hoangraper2017@gmail.com>
2025-08-08 18:26:08 +0700

    WIP: chỉnh sửa login page

---
+++ apps/web/src/app/auth/login/page.tsx
@@ -0,0 +1,14 @@
+// F:\say-2-hand\apps\backend\src\app\auth\login\page.tsx
+
+// Đây là một React Functional Component
+const LoginPage = () => {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      {/* Thêm form đăng nhập vào đây */}
+    </div>
+  );
+};
+
+// Phải export component này dưới dạng default export
+export default LoginPage;
