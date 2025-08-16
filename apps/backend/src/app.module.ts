f410d85cb9333ede47ed5815fe1c8f36f65b091b
DanielorRyker <hoangraper2017@gmail.com>
2025-08-16 11:14:38 +0700

    refactor: clean up app.module.ts and update package.json dependencies

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,19 +1,3 @@
-// import { Logger, Module } from '@nestjs/common';
-// import { MongooseModule } from '@nestjs/mongoose';
-// import { ConfigModule } from '@nestjs/config';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
-
-// @Module({
-//   imports: [
-//     ConfigModule.forRoot({ isGlobal: true }),
-//     MongooseModule.forRoot(process.env.MONGODB_URI || ''),
-//   ],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
-// export class AppModule {}
-
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
@@ -26,10 +10,10 @@ import { AuthModule } from './auth/auth.module';
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     // MongooseModule.forRoot(process.env.MONGODB_URI || ''),
-    
+
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
-      useFactory: async (configService: ConfigService) => ({
+      useFactory: (configService: ConfigService) => ({
         uri: configService.get<string>('MONGODB_URI'),
       }),
       inject: [ConfigService],
