ad9b054ae7533cee83bac80c5b7cd2f4c3ff1036
DanielorRyker <hoangraper2017@gmail.com>
2025-09-01 14:14:53 +0700

    chore(web): update next config and package.json

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['storage.googleapis.com'],
+    domains: ["storage.googleapis.com", "placehold.co"],
   },
   // Thêm các config khác nếu cần
 };
