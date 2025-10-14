1a3ea7d7c2d5c0f49c1d985117ecf1aa451ce16f
thanhnhan16 <thanhnhan16.2.2002@gmail.com>
2025-10-14 13:57:51 +0700

    (BE):fix slugify for upload image

--- apps/backend/src/modules/upload/upload.service.ts
+++ apps/backend/src/modules/upload/upload.service.ts
@@ -115,10 +115,11 @@ export class UploadService {
     async uploadImgs(files: Express.Multer.File[], folder: string) {
       const bucket = this.storage.bucket(this.bucketName);
 
-      const path = this.slugify(folder);
+      
       const uploadPromises = files.map((file) => {
+        const nameSlugify = this.slugify(folder);
         const random = Math.floor(100000 + Math.random() * 900000).toString();
-        const fileName = `${path}/${Date.now()}-${random}-${file.originalname}`;
+        const fileName = `${nameSlugify}/${Date.now()}-${random}-${file.originalname}`;
         const blob = bucket.file(fileName);
 
         const blobStream = blob.createWriteStream({
@@ -139,18 +140,18 @@ export class UploadService {
       return results; // Trả về mảng tên file
     }
 
-     slugify(input: string): string {
-        return input
-          .normalize('NFD') // tách dấu
-          .replace(/[\u0300-\u036f]/g, '') // xoá dấu tổ hợp
-          .replace(/đ/g, 'd') // xử lý chữ đ
-          .replace(/Đ/g, 'd') // xử lý chữ Đ
-          .toLowerCase()
-          .trim()
-          .replace(/\s+/g, '-') // space -> -
-          .replace(/[^a-z0-9-]/g, '') // loại ký tự đặc biệt
-          .replace(/-+/g, '-'); // gộp nhiều -
-      }
+    slugify(input: string): string {
+      return input
+        .normalize('NFD') // tách dấu
+        .replace(/đ/g, 'd') // xử lý chữ đ
+        .replace(/Đ/g, 'd') // xử lý chữ Đ
+        .toLowerCase()
+        .trim()
+        .replace(/\s+/g, '-') // space -> -
+        .replace(/[^a-z0-9\-\\/]/g, '') 
+        .replace(/-+/g, '-'); // gộp nhiều -
+    }
+
 
 }
 
